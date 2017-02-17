d2f7c80b9a68cd0d1607f8643a8e1951b1f871ce
JereNurminen <jere.m.nurminen@outlook.com>
2017-02-17 18:13:40 +0200

    The login works
    Finalized the registration/login-stuff

--- routes.js
+++ routes.js
@@ -19,7 +19,13 @@ module.exports = function(app, passport) {
     });
 
     // process the login form
-    // app.post('/login', do all our passport stuff here);
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
 
     // =====================================
     // SIGNUP ==============================
@@ -32,7 +38,11 @@ module.exports = function(app, passport) {
     });
 
     // process the signup form
-    // app.post('/signup', do all our passport stuff here);
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
     // =====================================
     // PROFILE SECTION =====================
--- server.js
+++ server.js
@@ -13,6 +13,8 @@ var configDB = require('./config/database.js');
 
 mongoose.connect(configDB.url);
 
+require('./config/passport')(passport);
+
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
@@ -26,6 +28,18 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 require('./routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// socket settings
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
 
 app.listen(port);
 console.log('The magic happens on port ' + port);
@@ -41,14 +55,5 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
-});
+
  */
